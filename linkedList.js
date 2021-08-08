7b48cad57eb1c77f7c6261a1e137b73d7a191304
AlexanderBowers <7319abowers@gmail.com>
2021-08-07 18:13:43 -0700

    added pop to linked list

--- linkedList.js
+++ linkedList.js
@@ -20,6 +20,38 @@ class SinglyLinkedList {
       this.length++
       return this.length
     }
+    pop(){
+        //if there is no head, return undefined 
+        if(this.head == null){
+          return undefined
+        }
+        //initialize two variables. The first variable will represent/assigned to this.head
+        let current = this.head
+        //the second will be assigned to the new variable, 
+        let newTail = current
+        //while the current.next is not equal to null[x], 
+        //assign the newTail to be the current [x] 
+        // the looping works by assigning the current = current.next 
+        while (current.next != null){
+          newTail = current
+          current = current.next
+        }
+        //assign the this.tail to be the newTail 
+        this.tail = newTail
+        //assign the .next value to be null 
+        this.tail.next = null
+        //decrement the length
+        this.length-- 
+        //if the length of the list is 0, reassign the head and tail to be null. 
+        //alternatively return undefined 
+        if (this.length == 0){
+          this.head = null
+          this.tail = null
+        }
+        //return the popped item off 
+        return current
+      }
+    
   
   
   }

a4ae95121a1abc0731a3b61c646e15e1d33d6e7d
AlexanderBowers <7319abowers@gmail.com>
2021-08-07 18:13:22 -0700

    basic linked list

---
+++ linkedList.js
@@ -0,0 +1,25 @@
+class SinglyLinkedList {
+    constructor(){
+      this.head = null
+      this.tail = null
+      this.length = 0
+    }
+    push(value){
+      //initialize a new node for every value that is pushed in
+      let newNode = new Node(value)
+      //if there is no head, assign the head and tail to be the newNode
+      if(this.head == null){
+        this.head = newNode
+        this.tail = newNode
+      } else{
+        //otherwise, create a link from the tail to a new point
+        //assign the tail to that point we made
+        this.tail.next = newNode
+        this.tail = newNode
+      }
+      this.length++
+      return this.length
+    }
+  
+  
+  }
